18a356adbe84d77aedfdfaa9f9fc516d9b0b2441
raindroid <yucan/wu@outlook.com>
2020-01-19 03:51:10 -0500

    lab2 init

---
+++ lab2/de1soc_top.sv
@@ -0,0 +1,39 @@
+module de1soc_top 
+(
+	// These are the board inputs/outputs required for all the ECE342 labs.
+	// Each lab can use the subset it needs -- unused pins will be ignored.
+	
+    // Clock pins
+    input                     CLOCK_50,
+
+    // Seven Segment Displays
+    output      [6:0]         HEX0,
+    output      [6:0]         HEX1,
+    output      [6:0]         HEX2,
+    output      [6:0]         HEX3,
+    output      [6:0]         HEX4,
+    output      [6:0]         HEX5,
+
+    // Pushbuttons
+    input       [3:0]         KEY,
+
+    // LEDs
+    output      [9:0]         LEDR,
+
+    // Slider Switches
+    input       [9:0]         SW,
+
+    // VGA
+    output      [7:0]         VGA_B,
+    output                    VGA_BLANK_N,
+    output                    VGA_CLK,
+    output      [7:0]         VGA_G,
+    output                    VGA_HS,
+    output      [7:0]         VGA_R,
+    output                    VGA_SYNC_N,
+    output                    VGA_VS
+);
+	
+	// Design goes here
+	
+endmodule
