e9453795a46d2a23172715ced5709a7f63f20071
raindroid <yucan.wu@outlook.com>
2020-02-21 05:44:10 -0500

    作业好多

---
+++ lab5/start/cpu.sv
@@ -0,0 +1,19 @@
+//REVIEW  CPU moduls
+
+module cpu (
+    input clk,
+    input reset,
+
+    output logic [15:0] o_mem_addr,
+    output logic        o_mem_rd,
+    input[15:0]         o_mem_rddata,
+    output logic        o_mem_wr,
+    output logic [15:0] o_mem_wrdata
+);
+
+    // Datapath
+
+
+    // Control FSM
+    
+endmodule
